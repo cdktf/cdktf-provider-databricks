fa44c38518720c30d29e15f12ac64029d2cec0ec
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-06 14:07:05 +0000

    chore(deps): Updated

--- src/data-databricks-service-principals/index.ts
+++ src/data-databricks-service-principals/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.20.0/docs/data-sources/service_principals
 // generated from terraform resource schema
 
--- src/permissions/index.ts
+++ src/permissions/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.20.0/docs/resources/permissions
 // generated from terraform resource schema
 
