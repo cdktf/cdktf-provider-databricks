f298844d9c979b65759bcf79a1692675fac365f4
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-10-13 12:50:49 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@f0b85b7

--- src/external-location/index.ts
+++ src/external-location/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.27.0/docs/resources/external_location
 // generated from terraform resource schema
 
