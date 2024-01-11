9bdb2f175c7764617f8044d7040776543c930d32
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-11 18:39:44 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/azure-blob-mount/index.ts
+++ src/azure-blob-mount/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/databricks/databricks/1.34.0/docs/resources/azure_blob_mount
 // generated from terraform resource schema
 
