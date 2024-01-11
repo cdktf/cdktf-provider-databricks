0196722419bd2f4ba2ab725c802ba28d69942827
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-11 18:37:04 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@e657ab6

--- src/secret-scope/index.ts
+++ src/secret-scope/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.34.0/docs/resources/secret_scope
 // generated from terraform resource schema
 
