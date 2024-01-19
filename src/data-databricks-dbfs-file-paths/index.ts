052e49b1aa3627e948cc93db5986f2e9cba0d929
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-01-19 12:13:00 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-databricks-dbfs-file-paths/index.ts
+++ src/data-databricks-dbfs-file-paths/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/databricks/databricks/1.34.0/docs/data-sources/dbfs_file_paths
 // generated from terraform resource schema
 
