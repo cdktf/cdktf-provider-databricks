94ce14fd846d7c8d53c9a0d6470a22aa97deba0a
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-03-13 13:46:49 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@00b17fe

--- src/directory/index.ts
+++ src/directory/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.38.0/docs/resources/directory
 // generated from terraform resource schema
 
