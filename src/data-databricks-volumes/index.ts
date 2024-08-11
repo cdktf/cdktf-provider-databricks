2dd6bb19c3ae5c0d09faaddf6aeda7f0653eac60
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-11 21:52:43 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2e1259a

--- src/data-databricks-volumes/index.ts
+++ src/data-databricks-volumes/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.49.1/docs/data-sources/volumes
 // generated from terraform resource schema
 
