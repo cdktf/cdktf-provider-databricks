f99e18c5535de05bd7e373e3adeedf16550cd97a
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-22 05:55:28 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-databricks-mlflow-experiment/index.ts
+++ src/data-databricks-mlflow-experiment/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/databricks/databricks/1.52.0/docs/data-sources/mlflow_experiment
 // generated from terraform resource schema
 
