e25492267cef7c3f4cc5f4d280ad3f9ee4f6a0b3
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-27 05:55:42 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/artifact-allowlist/index.ts
+++ src/artifact-allowlist/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/databricks/databricks/1.55.0/docs/resources/artifact_allowlist
 // generated from terraform resource schema
 
