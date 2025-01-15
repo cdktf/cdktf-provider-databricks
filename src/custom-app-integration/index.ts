e720e3f27cbe47cc164d08d70ab654f41a1f6c70
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-15 12:17:49 +0000

    chore(deps): update  JSII version to

--- src/custom-app-integration/index.ts
+++ src/custom-app-integration/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.62.1/docs/resources/custom_app_integration
 // generated from terraform resource schema
 
