2d08b537f797b2794d4cfd8af14e922adbe689d0
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-02-02 05:53:40 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-databricks-aws-unity-catalog-assume-role-policy/index.ts
+++ src/data-databricks-aws-unity-catalog-assume-role-policy/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/databricks/databricks/1.64.1/docs/data-sources/aws_unity_catalog_assume_role_policy
 // generated from terraform resource schema
 
