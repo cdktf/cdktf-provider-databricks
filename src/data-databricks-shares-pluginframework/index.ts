9868c202598637f201f230974e8b410e6bd647ac
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-03-27 21:55:45 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@2db01f0

--- src/data-databricks-shares-pluginframework/index.ts
+++ src/data-databricks-shares-pluginframework/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.70.0/docs/data-sources/shares_pluginframework
 // generated from terraform resource schema
 
