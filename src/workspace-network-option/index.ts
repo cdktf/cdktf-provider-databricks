f1312960311358a48d1bc64b14174f5d5a424c67
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:35:32 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@909bd84

--- src/workspace-network-option/index.ts
+++ src/workspace-network-option/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.81.1/docs/resources/workspace_network_option
 // generated from terraform resource schema
 
