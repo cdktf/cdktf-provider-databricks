2aeef7339846ad180858115aaaf552a49db7da88
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 14:42:41 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@4d35a18

--- src/credential/index.ts
+++ src/credential/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/databricks/databricks/1.81.1/docs/resources/credential
 // generated from terraform resource schema
 
